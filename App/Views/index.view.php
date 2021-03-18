b2dd625be1c6a329c3b9bb6d60b2b21e3c1ee6ca
chamodya wimansha <chamodyawimansha@gmail.com>
2021-03-18 15:32:39 +0530

    updates gitignore file

--- App/Views/index.view.php
+++ App/Views/index.view.php
@@ -16,9 +16,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?php "styles.css" ?>">
     <title>Index</title>
 </head>
 <body>
-    <h1><?php $viewData["demo"]; ?></h1>
+    <h1><?php echo $viewData["demo"]; ?></h1>
 </body>
 </html>
