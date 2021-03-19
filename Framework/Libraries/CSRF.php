57a93c5ddf41230122749b3eab542bd41f81b4ee
chamodya wimansha <chamodyawimansha@gmail.com>
2021-03-19 19:26:50 +0530

    session_write_close on Application destruct

---
+++ Framework/Libraries/CSRF.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * CSRF class File
+ * php version 7.4.2
+ *
+ * @category Controller_Class
+ * @package  SimplePHP
+ * @author   Chamodya Wimansha <chamodyawimansha@gmail.com>
+ * @license  http://www.gnu.org/copyleft/gpl.html GNU Gene
+ * @link     https://github.com/chamodyawimansha/SimplePHP
+ */
+
+namespace Framework\Libraries;
+
+/**
+ * CSRF Class - Generates and manages one-time token
+ * php version 7.4.2
+ *
+ * @category Library_Class
+ * @package  SimplePHP
+ * @author   Chamodya Wimansha <chamodyawimansha@gmail.com>
+ * @license  http://www.gnu.org/copyleft/gpl.html GNU Gene
+ * @link     https://github.com/chamodyawimansha/SimplePHP
+ */
+class CSRF
+{
+
+    /**
+     * Creates a new Token
+     * 
+     * return 
+     */
+    public static function new()
+    {
+
+    }
+}
