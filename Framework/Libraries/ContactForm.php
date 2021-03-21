82b80e877a6da09d21997e759ca5481e904fc965
chamodya wimansha <chamodyawimansha@gmail.com>
2021-03-21 14:41:12 +0530

    Replaces the Autoload with Composer autoload

--- Framework/Libraries/ContactForm.php
+++ Framework/Libraries/ContactForm.php
@@ -12,10 +12,7 @@
 
  namespace Framework\Libraries;
 
- require ROOT . "vendor/pear/mail/Mail.php";
-
- use Mail;
-
+ require "Mail.php";
 /**
  * ContactForm Class - Uses the Pear::Mail class to send emails 
  * php version 7.4.2
@@ -37,6 +34,42 @@ class ContactForm
     public function __construct()
     {
 
+        $host = "smtp.gmail.com";
+        $port = 587;
+        $username = "duobrothers.wordpress@gmail.com";
+        $password = "2^FtGp7$";
+
+
+        $from = "website <chamodya@duobrothers.com>";
+        $to = "Me <chamodyawimansha@gmail.com>";
+        $replyto = "Visitor <reply-to@your-email-here.com>";
+
+
+        $subject = "Hi!";
+        $body = "Hi,\n\n Test from my website";
+
+
+        $headers = array ('From' => $from,
+        'To' => $to,
+        'Subject' => $subject,
+        'Reply-To' =>$replyto);
+        
+        $smtp = Mail::factory(
+            'smtp',
+            array ('host' => $host,
+            'port' => $port,
+            'auth' => true,
+            'username' => $username,
+            'password' => $password)
+        );
+
+        $mail = $smtp->send($to, $headers, $body);
+
+        if (PEAR::isError($mail)) {
+                echo("<p>" . $mail->getMessage() . "</p>");
+        } else {
+                echo("<p>Message successfully sent!</p>");
+        }
     }
 
 }
